b43317c9882c1c6d51278244535059fa8d8db4d4
stratixx <darnokusex@wp.pl>
2019-03-11 17:46:42 +0100

    zad4

---
+++ Lab1/zad_4/pid_init.m
@@ -0,0 +1,28 @@
+
+% parametry PID moje
+Kk = 199.33;
+Tk = 0.1;
+Tv = -0.0001;
+
+% ziegler grocha
+%Kk = 35.0;
+%Tk = 0.2;
+%Tv = -0.050
+
+% ziegler
+Kr = 0.6*Kk;
+Ti = 0.5*Tk;
+Td = 0.12*Tk;
+
+%rêcznie moje
+%Kr = 1;
+%Ti = 0.45;
+%Td = 0;
+
+%Kr = 8;
+%Ti = 0.07;
+%Td = 1.3;
+
+r0 = Kr*(1+Tp/(2*Ti)+Td/Tp);
+r1 = Kr*(Tp/(2*Ti)-2*Td/Tp-1);
+r2 = Kr*Td/Tp;
