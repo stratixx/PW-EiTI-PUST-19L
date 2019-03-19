d5c8f0d641b6d2cd5d0f159e840d23606eaded3b
grochu10 <mati.groszek9@interia.pl>
2019-03-19 22:27:32 +0100

    zad1 all

--- zadanie1/projekt/zad1/zad1.m
+++ zadanie1/projekt/zad1/zad1.m
@@ -1,18 +1,21 @@
-run('danePoczatkowe.m');
+run('../danePoczatkowe.m');
 start = 20;
-kk=210; %koniec symulacji
+kk=300; %koniec symulacji
 %warunki pocztkowe
-u=ones(1,kk)*0.8;
-y=zeros(1,kk);
-e=zeros(1,kk);
+u = ones(1,kk)*Upp;
+y = zeros(1,kk);
+y(1,1:start) = Ypp;
+e = zeros(1,kk);
 
-for k=start:kk; %g��wna ptla symulacyjna
+for k=start:kk %g��wna ptla symulacyjna
     %symulacja obiektu   
     y(k) = symulacja_obiektu7Y(u(k-10), u(k-11), y(k-1), y(k-2));
 end
-figure(2); 
+figure; 
 hold on; 
 grid on; 
-plot(y(start:end));  
-plot(u(start:end));  
-legend('y', 'u')
+plot(y);    
+legend('y')
+figure;
+plot(u);
+legend('u')
