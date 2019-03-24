180591d2b300024c16b8a0c32efbfffb75a380ec
grochu10 <mati.groszek9@interia.pl>
2019-03-24 16:48:14 +0100

    char stat legitna

--- zadanie2/zad2/char_stat.m
+++ zadanie2/zad2/char_stat.m
@@ -6,29 +6,20 @@ Upp = 0;
 Ypp = 0;
 Zpp = 0;
 Y = zeros(21);
-i1 =1;
-j1=1;
-for i = -2:0.2:2
-    for j = -2:0.2:2
+u1 = -2:0.05:2;
+z1 = -2:0.05:2;
+for i = 1:length(u1)
+    for j = 1:length(z1)
     y = ones(1,kk)*Ypp;
-    u = ones(1,kk)*Upp; 
-    u(1,start:kk) = i;
-    z = ones(1,kk)*Upp; 
-    z(1,start:kk) = j;
+    u = ones(1,kk)*u1(i); 
+    z = ones(1,kk)*z1(j); 
     for k=start:kk %glowna petla symulacyjna
         %symulacja obiektu   
         y(k)=symulacja_obiektu7y(u(k-4),u(k-5),z(k-1),z(k-2),y(k-1),y(k-2));
     end
-    Y(i1,j1) = y(end);
-    j1 = j1+1;
+    Y(i,j) = y(k);
     end
-    i1 = i1+1;
-    j1 = 1;
 end
-hold on; 
-grid on;
-u1 = -2:0.2:2;
-z1 = -2:0.2:2;
 surf(u1,z1,Y);
 xlabel('U')
 ylabel('Z')
