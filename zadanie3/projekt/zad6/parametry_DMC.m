692a2f8fa872d4fc962e65e643303362967c14bc
grochu10 <mati.groszek9@interia.pl>
2019-04-18 23:45:04 +0200

    zad6 all kurwa

--- zadanie3/projekt/zad6/parametry_DMC.m
+++ zadanie3/projekt/zad6/parametry_DMC.m
@@ -1,9 +1,8 @@
-function [Mp,K,u_delta] = parametry_DMC(D,N,Nu,fuzzyN,k)
-lambda = 1;
-if k == 1
+function [Mp,K,u_delta] = parametry_DMC(D,N,Nu,lambda,fuzzyN,k)
+if k < 2
     load('s21.mat')
 end
-if k == 2
+if k >1
     load('s22.mat')
 end
 Mp = zeros(N,D - 1);
@@ -18,11 +17,11 @@ for i = 1:N
 end
 M = zeros(N,Nu);
 for i = 1:N
-for j = 1:Nu
-  if (i >= j)
-     M(i,j) = s(i-j+1);
-  end
-end
+    for j = 1:Nu
+      if (i >= j)
+         M(i,j) = s(i-j+1);
+      end
+    end
 end
 mac_lam = lambda*eye(Nu);
 psi = eye(N);
--- zadanie3/projekt/zad6/zad6_DMC.m
+++ zadanie3/projekt/zad6/zad6_DMC.m
@@ -3,7 +3,6 @@
 % D = 70;%horyzont dynamiki
 % N = 50;%horyzont predykcji
 % Nu = 6;%horyzont sterowania
-lambda = 1;
 kk = 1800;
 y_zad=zeros(1,kk);
 y_zad(1,20:200) = -2;
@@ -16,20 +15,19 @@ y_zad(1,1200:1400) = -0.3;
 y_zad(1,1400:1600) = 0;
 y_zad(1,1600:kk) = 0.1;
 
-fuzzyN = 2;%ilosc regulatorow
-[D,N,Nu] = nastawy_DMC(fuzzyN);%nastawy dla opd. ilosci regulatorow
+fuzzyN = 5;%ilosc regulatorow
+[D,N,Nu,lambda] = nastawy_DMC(fuzzyN);%nastawy dla opd. ilosci regulatorow
 settings = [];
 
 for n=1:fuzzyN
     settings(n).D = D(n);
     settings(n).N = N(n);
     settings(n).Nu = Nu(n);
-    [Mp,K,u_delta] = parametry_DMC(settings(n).D,settings(n).N,settings(n).Nu,fuzzyN,n);
+    settings(n).lambda = lambda(n);
+    [Mp,K,u_delta] = parametry_DMC(settings(n).D,settings(n).N,settings(n).Nu,settings(n).lambda,fuzzyN,n);
     settings(n).Mp = Mp;
     settings(n).K = K;
     settings(n).u_delta = u_delta;
-	settings(n).lambda = 1;
-    settings(n).u1 = 0;
 end
 
 %warunki poczatkowe
@@ -46,13 +44,12 @@ y_mod = zeros(kk,1);
 for k = 20:kk
    % Równanie ró?nicowe z puntku drugiego
    y_mod(k)=symulacja_obiektu7y(u(k-5),u(k-6),y_mod(k-1),y_mod(k-2));  
-   
+   u_ = zeros(1,fuzzyN);
    for n = 1:fuzzyN
    % Regulator 
        for i = settings(n).D-1:-1:2
            settings(n).u_delta(i) = settings(n).u_delta(i-1);
        end
-       u_ = zeros(1, fuzzyN);
        uchyb = y_zad(k) - y_mod(k);
        settings(n).u_delta(1) = sum(settings(n).K(1,:))*uchyb - settings(n).K(1,:)*settings(n).Mp*settings(n).u_delta';   
        u_(n) = u(k-1) + settings(n).u_delta(1);
@@ -97,6 +94,6 @@ stairs(u,'c')
 title('Sterowanie modelu')
 ylabel('u')
 xlabel('k');
-% dlmwrite('../Dane/Zad6/DMC/N2/u.csv', u, '\t');
-% dlmwrite('../Dane/Zad6/DMC/N2/y.csv', y_mod, '\t');
-% dlmwrite('../Dane/Zad6/DMC/N2/y_zad.csv', y_zad, '\t');
+dlmwrite('../Dane/Zad6/DMC/N5/u.csv', u, '\t');
+dlmwrite('../Dane/Zad6/DMC/N5/y.csv', y_mod, '\t');
+dlmwrite('../Dane/Zad6/DMC/N5/y_zad.csv', y_zad, '\t');
