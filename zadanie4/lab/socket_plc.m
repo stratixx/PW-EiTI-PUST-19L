e800c9b9b3385ed3abf718332bd2fe8c77e0f468
stratixx_CS402_Stage2 <darnokusex@wp.pl>
2019-05-13 18:00:00 +0200

    po labce 2

--- zadanie4/lab/socket_plc.m
+++ zadanie4/lab/socket_plc.m
@@ -28,34 +28,52 @@ g2=[];
 t1=[];
 t2=[];
 t3=[];
+k=[];
 
 figure(1);
-while (length(y1) < 100)
+while (length(g1) < 10000)
     if (t.BytesAvailable ~= 0)
         temp = fscanf(t);
         disp(temp);
         eval(temp);
         
         
-        w1 = [w1; W1];
-        w2 = [w2; W2];
-        w3 = [w3; W3];
-        w4 = [w4; W4];
+%         w1 = [w1; W1];
+%         w2 = [w2; W2];
+%         w3 = [w3; W3];
+%         w4 = [w4; W4];
         g1 = [g1; G1];
         g2 = [g2; G2];
         t1 = [t1; T1];
         t2 = [t2; T2];
-        t3 = [t3; T3];
+         t3 = [t3; T3];
+         k=[k;K];
         
-        y1 = t1;
-        y2 = t2;
+        y1 = t1/100;
+        y2 = t3/100;
         
-        u1 = g1;
-        u2 = g2;
+        u1 = g1/10;
+        u2 = g2/10;
      
         
-        subplot(2,1,1); plot(y1); hold on; plot(y2); hold off; title('Wyjœcie'); xlabel('iteracja');
-        subplot(2,1,2); stairs(u1); hold on; stairs(u2); hold off; title('Sterowanie'); xlabel('iteracja');
+        subplot(2,1,1); 
+        stairs(y1); 
+        hold on; 
+        stairs(y2); 
+        hold off; 
+        title('Wyjœcie'); 
+        xlabel('iteracja');
+        legend('T1', 'T3');
+        
+        subplot(2,1,2); 
+        stairs(u1); 
+        hold on; 
+        stairs(u2); 
+        hold off; 
+        title('Sterowanie'); 
+        xlabel('iteracja');
+        legend('G1', 'G2');
+        
         drawnow
     end
     pause(0.05);
