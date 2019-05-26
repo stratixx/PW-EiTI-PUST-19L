4ca1569a822860ae257376af21a51121c99a3a20
stratixx <darnokusex@wp.pl>
2019-05-27 00:38:44 +0200

    generacja danych DMC; program DMC na PLC

---
+++ zadanie4/lab/DMC/DMC_script.m
@@ -0,0 +1,42 @@
+% Plik:			DMC_script.m
+% Autor:		Konrad Winnicki
+% E-mail:		konrad_winnicki@wp.pl
+% Przedmiot:	PUST
+% Semestr:		19L
+% Opis:			Skrypt wyliczający parametry regulatora DMC
+%				przeznaczonego do uruchomienia w systemie wbudowanym
+
+% Założone parametry regulatora
+lam = 0.2
+load('../zad1/esy/s1.mat')
+s = s1aprox;
+D1 = length(s); % horyzont dynamiki
+N1=D1;
+Nu1=N1;
+lambda1 = lam;
+D = D1; % horyzont dynamiki
+N=N1;
+Nu=Nu1;
+lambda = lambda1;
+run('DMC_init.m');
+
+Ke1 = sum(K(1,:));
+Ku1 = K(1,:)*Mp;
+
+load('../zad1/esy/s2.mat')
+s = s2aprox;
+D2 = length(s); % horyzont dynamiki
+N2=D2;
+Nu2=N2;
+lambda2 = lam;
+D = D2; % horyzont dynamiki
+N=N2;
+Nu=Nu2;
+lambda = lambda2;
+run('DMC_init.m');
+
+Ke2 = sum(K(1,:));
+Ku2 = K(1,:)*Mp;
+
+% wyeksportowanie wyznaczonych parametrów do pliku nagłówkowego zgodnego ze standardem języka C
+run('exporter.m');
---
+++ zadanie4/lab/DMC/exporter.m
@@ -0,0 +1,49 @@
+% Plik:			exporter.m
+% Autor:		Konrad Winnicki
+% E-mail:		konrad_winnicki@wp.pl
+% Przedmiot:	PUST
+% Semestr:		19L
+% Opis:			Skrypt eksportujący wyliczone parametry regulatora DMC do postaci
+%				zgodnej ze standardem języka ST
+
+% powstanie plik "DMC_data.h" w folderze Inc
+fileID = fopen('DMC_data.st','w');
+
+fprintf(fileID,'//Parametry regulatora DMC1\n');
+fprintf(fileID,'DMC_G1.D := %d;\n', D1);
+fprintf(fileID,'DMC_G1.N := %d;\n', N1);
+fprintf(fileID,'DMC_G1.Nu := %d;\n', Nu1);
+fprintf(fileID,'DMC_G1.lambda := %f;\n\n', lambda1);
+
+fprintf(fileID,'//Parametry regulatora DMC2\n');
+fprintf(fileID,'DMC_G2.D := %d;\n', D2);
+fprintf(fileID,'DMC_G2.N := %d;\n', N2);
+fprintf(fileID,'DMC_G2.Nu := %d;\n', Nu2);
+fprintf(fileID,'DMC_G2.lambda := %f;\n\n', lambda2);
+
+fprintf(fileID,'// Przeliczone wartosci do sterowania DMC1\n');
+fprintf(fileID,'DMC_G1.Ke := %f;\n\n', Ke1);
+fprintf(fileID,'// Przeliczone wartosci do sterowania DMC2\n');
+fprintf(fileID,'DMC_G2.Ke := %f;\n\n', Ke2);
+
+for n=1:length(Ku1)    
+    fprintf(fileID,'DMC_G1.Ku[%d] := %f;\n', n-1, Ku1(n));
+end
+fprintf(fileID,'\n');
+    
+for n=1:length(Ku2)    
+    fprintf(fileID,'DMC_G2.Ku[%d] := %f;\n', n-1, Ku2(n));
+end
+fprintf(fileID,'\n');
+
+for n=1:length(Ku1)    
+    fprintf(fileID,'DMC_G1.delta_u_past[%d] := %f;\n', n-1, 0.0);
+end
+fprintf(fileID,'\n');
+
+for n=1:length(Ku2)    
+    fprintf(fileID,'DMC_G2.delta_u_past[%d] := %f;\n', n-1, 0.0);
+end
+fprintf(fileID,'\n');
+
+fclose(fileID);
